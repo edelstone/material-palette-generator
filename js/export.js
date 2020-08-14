de108b000b0fed5e289603d9692568653f811132
Ziya <ziya.fenn@gmail.com>
2020-08-14 16:34:09 +0200

    Add Color Label to the export

--- js/export.js
+++ js/export.js
@@ -3,36 +3,67 @@ function ExportColor() {
     const colorSelector = document.querySelectorAll(".color-palette__cell-hex-value");
     const colors = [...colorSelector];
     const textArea = document.querySelector("#jsonText");
+    const colorLabels = ["900", "800", "700", "600", "500", "400", "300", "200", "100", "50"]
     let exported = {};
 
     if (colorPalettes.length > 2) {
+      let labelIndex = 0;
         colorPalettes.forEach((palette, paletteIndex) => {
           if (paletteIndex === 0) {
             const filteredColors = colors.filter((color, colorIndex) =>  colorIndex < 10)
-            filteredColors.forEach((hex, hexIndex) => exported["Primary"] = {...exported["Primary"], [hexIndex + 1]: hex.textContent})
+            filteredColors.forEach((hex, hexIndex) => {exported["Primary"] = {...exported["Primary"], [colorLabels[labelIndex]]: hex.textContent}
+            labelIndex++})
           }
           if (paletteIndex === 1) {
+            let labelIndex = 0;
             const filteredColors = colors.filter((color, colorIndex) =>  colorIndex >= 10 && colorIndex < 20)
-            filteredColors.forEach((hex, hexIndex) => exported["Complementary"] = {...exported["Complementary"], [hexIndex + 1]: hex.textContent})
+            filteredColors.forEach((hex, hexIndex) => {exported["Complementary"] = {...exported["Complementary"], [colorLabels[labelIndex]]: hex.textContent}
+            labelIndex++})
           }
           if (paletteIndex === 2) {
-            const filteredColors = colors.filter((color, colorIndex) =>  colorIndex >= 20 && colorIndex < 40)
-            filteredColors.forEach((hex, hexIndex) => exported["Analogous"] = {...exported["Analogous"], [hexIndex + 1]: hex.textContent})
+            let labelIndex = 0;
+            const filteredColors = colors.filter((color, colorIndex) =>  colorIndex >= 20 && colorIndex < 30)
+            filteredColors.forEach((hex, hexIndex) => {exported["Analogous - 1"] = {...exported["Analogous - 1"], [colorLabels[labelIndex]]: hex.textContent}
+            labelIndex++
+          })
           }
           if (paletteIndex === 3) {
-            const filteredColors = colors.filter((color, colorIndex) =>  colorIndex >= 40)
-            filteredColors.forEach((hex, hexIndex) => exported["Triadic"] = {...exported["Triadic"], [hexIndex + 1]: hex.textContent})
+            let labelIndex = 0;
+            const filteredColors = colors.filter((color, colorIndex) =>  colorIndex >= 30 && colorIndex < 40)
+            filteredColors.forEach((hex, hexIndex) => {exported["Analogous - 2"] = {...exported["Analogous - 2"], [colorLabels[labelIndex]]: hex.textContent}
+            labelIndex++
+          })
+          }
+          if (paletteIndex === 4) {
+            let labelIndex = 0;
+            const filteredColors = colors.filter((color, colorIndex) =>  colorIndex >= 40 && colorIndex < 50)
+            filteredColors.forEach((hex, hexIndex) => {exported["Triadic - 1"] = {...exported["Triadic - 1"], [colorLabels[labelIndex]]: hex.textContent}
+            labelIndex++
+          })
+          }
+          if (paletteIndex === 5) {
+            let labelIndex = 0;
+            const filteredColors = colors.filter((color, colorIndex) =>  colorIndex >= 50)
+            filteredColors.forEach((hex, hexIndex) => {exported["Triadic - 2"] = {...exported["Triadic - 2"], [colorLabels[labelIndex]]: hex.textContent}
+            labelIndex++
+          })
           }
          
         })
     } else {
         colorPalettes.forEach((palette, paletteIndex) => {
             if (paletteIndex === 0) {
+              let labelIndex = 0;
                 const filteredColors = colors.filter((color, colorIndex) =>  colorIndex < 10)
-                filteredColors.forEach((hex, hexIndex) => exported["Primary"] = {...exported["Primary"], [hexIndex + 1]: hex.textContent})
+                filteredColors.forEach((hex, hexIndex) => {exported["Primary"] = {...exported["Primary"], [colorLabels[labelIndex]]: hex.textContent}
+                labelIndex++
+              })
             } else {
+              let labelIndex = 0;
                 const filteredColors = colors.filter((color, colorIndex) =>  colorIndex >= 10)
-                filteredColors.forEach((hex, hexIndex) => exported["Secondary"] = {...exported["Secondary"], [hexIndex + 1]: hex.textContent})
+                filteredColors.forEach((hex, hexIndex) => {exported["Secondary"] = {...exported["Secondary"], [colorLabels[labelIndex]]: hex.textContent}
+                labelIndex++
+              })
             }
     })}
 
