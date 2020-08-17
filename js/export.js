35904e05b03c716855f43ebea9becb6453186c76
Ziya Fenn <314302+ziyafenn@users.noreply.github.com>
2020-08-17 10:38:52 +0200

    Export palette as a JSON (#5)
    * Export color palette function
    * Add Button and Modal, style and output as a JSON inside textarea
    * Add Color Label to the export
    * "dialog" changes to support tabindex and focus
    * linter fix (use double space instead of tab)
    
    Closes #4

---
+++ js/export.js
@@ -0,0 +1,160 @@
+function ExportColor() {
+  const colorPalettes = document.querySelectorAll(".color-palette__row");
+  const colorSelector = document.querySelectorAll(
+    ".color-palette__cell-hex-value"
+  );
+  const colors = [...colorSelector];
+  const textArea = document.querySelector("#jsonText");
+  const COLOR_LABELS = [
+    "900",
+    "800",
+    "700",
+    "600",
+    "500",
+    "400",
+    "300",
+    "200",
+    "100",
+    "50",
+  ];
+  let exported = {};
+
+  if (colorPalettes.length > 2) {
+    let labelIndex = 0;
+    colorPalettes.forEach((palette, paletteIndex) => {
+      if (paletteIndex === 0) {
+        const filteredColors = colors.filter(
+          (color, colorIndex) => colorIndex < 10
+        );
+        filteredColors.forEach((hex, hexIndex) => {
+          exported["Primary"] = {
+            ...exported["Primary"],
+            [COLOR_LABELS[labelIndex]]: hex.textContent,
+          };
+          labelIndex++;
+        });
+      }
+      if (paletteIndex === 1) {
+        let labelIndex = 0;
+        const filteredColors = colors.filter(
+          (color, colorIndex) => colorIndex >= 10 && colorIndex < 20
+        );
+        filteredColors.forEach((hex, hexIndex) => {
+          exported["Complementary"] = {
+            ...exported["Complementary"],
+            [COLOR_LABELS[labelIndex]]: hex.textContent,
+          };
+          labelIndex++;
+        });
+      }
+      if (paletteIndex === 2) {
+        let labelIndex = 0;
+        const filteredColors = colors.filter(
+          (color, colorIndex) => colorIndex >= 20 && colorIndex < 30
+        );
+        filteredColors.forEach((hex, hexIndex) => {
+          exported["Analogous - 1"] = {
+            ...exported["Analogous - 1"],
+            [COLOR_LABELS[labelIndex]]: hex.textContent,
+          };
+          labelIndex++;
+        });
+      }
+      if (paletteIndex === 3) {
+        let labelIndex = 0;
+        const filteredColors = colors.filter(
+          (color, colorIndex) => colorIndex >= 30 && colorIndex < 40
+        );
+        filteredColors.forEach((hex, hexIndex) => {
+          exported["Analogous - 2"] = {
+            ...exported["Analogous - 2"],
+            [COLOR_LABELS[labelIndex]]: hex.textContent,
+          };
+          labelIndex++;
+        });
+      }
+      if (paletteIndex === 4) {
+        let labelIndex = 0;
+        const filteredColors = colors.filter(
+          (color, colorIndex) => colorIndex >= 40 && colorIndex < 50
+        );
+        filteredColors.forEach((hex, hexIndex) => {
+          exported["Triadic - 1"] = {
+            ...exported["Triadic - 1"],
+            [COLOR_LABELS[labelIndex]]: hex.textContent,
+          };
+          labelIndex++;
+        });
+      }
+      if (paletteIndex === 5) {
+        let labelIndex = 0;
+        const filteredColors = colors.filter(
+          (color, colorIndex) => colorIndex >= 50
+        );
+        filteredColors.forEach((hex, hexIndex) => {
+          exported["Triadic - 2"] = {
+            ...exported["Triadic - 2"],
+            [COLOR_LABELS[labelIndex]]: hex.textContent,
+          };
+          labelIndex++;
+        });
+      }
+    });
+  } else {
+    colorPalettes.forEach((palette, paletteIndex) => {
+      if (paletteIndex === 0) {
+        let labelIndex = 0;
+        const filteredColors = colors.filter(
+          (color, colorIndex) => colorIndex < 10
+        );
+        filteredColors.forEach((hex, hexIndex) => {
+          exported["Primary"] = {
+            ...exported["Primary"],
+            [COLOR_LABELS[labelIndex]]: hex.textContent,
+          };
+          labelIndex++;
+        });
+      } else {
+        let labelIndex = 0;
+        const filteredColors = colors.filter(
+          (color, colorIndex) => colorIndex >= 10
+        );
+        filteredColors.forEach((hex, hexIndex) => {
+          exported["Secondary"] = {
+            ...exported["Secondary"],
+            [COLOR_LABELS[labelIndex]]: hex.textContent,
+          };
+          labelIndex++;
+        });
+      }
+    });
+  }
+
+  textArea.textContent = JSON.stringify(exported, null, "  ");
+  modal.showModal();
+}
+
+// Controls
+
+const modal = document.getElementById("exportModal");
+const exportButton = document.querySelector(".close");
+const contentArea = document.querySelector(".color-tool");
+
+contentArea.insertAdjacentHTML(
+  "beforeend",
+  `
+<div class="buttonContainer"><button class="exportButton" tabindex="1" onclick="ExportColor()">Export Palette</button></div>
+`
+);
+
+// When the user clicks on <span> (x), close the modal
+exportButton.onclick = function () {
+  modal.close();
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.close();
+  }
+};
